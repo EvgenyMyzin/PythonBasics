740e7677f9299a0155bfec6b0f93c7238a65088f
EvgenyMyzin <evgeny.myzin@gmail.com>
2021-03-04 16:22:56 +0300

    Second attempt

---
+++ Lesson 3 Task 1.py
@@ -0,0 +1,16 @@
+"""Homework 3 Task 1
+
+    1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление. 
+    Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+
+    """
+
+def first_func(var_1, var_2):
+    if var_2==0.0:
+        return "division by zero"
+    return var_1/var_2
+
+num = float(input("NUM: "))
+denom = float(input("DENOM: "))
+print(first_func(num, denom))
+
